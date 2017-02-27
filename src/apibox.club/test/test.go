f8e448ddb3ddb4900752e99a6f5859ae940e560b
shibingli <shibingli@yeah.net>
2017-02-27 15:25:28 +0800

    Update  conf.json

--- src/apibox.club/test/test.go
+++ src/apibox.club/test/test.go
@@ -2,36 +2,18 @@ package main
 
 import (
 	"apibox.club/utils"
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	// "os"
-	// "strings"
-	"syscall"
+	"net"
+	"net/url"
+	"strings"
 )
 
-func GetPID() (int, error) {
-	b, err := ioutil.ReadFile(apibox.PidPath)
-	if nil != err {
-		return -1, err
-	}
-	b = bytes.TrimSpace(b)
-	pid, err := apibox.StringUtils(string(b)).Int()
-	if nil != err {
-		return -1, err
-	}
-	return pid, nil
-}
-
 func main() {
-	pid, err := GetPID()
+
+	//golang 1.8 bug
+	u, err := url.Parse("192.168.2.2:6688")
 	if nil != err {
 		panic(err)
 	}
-
-	if err := syscall.Kill(pid, 0); nil != err {
-		fmt.Println(err.Error())
-	} else {
-		fmt.Println("sssss")
-	}
+	fmt.Println(u.Host, u.Port())
 }
