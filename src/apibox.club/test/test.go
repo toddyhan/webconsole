59e0749d3d4bb17622ab783c4cb09b763881340f
shibingli <shibingli@yeah.net>
2017-02-27 17:03:42 +0800

    Update README

--- src/apibox.club/test/test.go
+++ src/apibox.club/test/test.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"apibox.club/utils"
 	"fmt"
-	"net"
 	"net/url"
-	"strings"
 )
 
 func main() {
 
 	//golang 1.8 bug
-	u, err := url.Parse("192.168.2.2:6688")
+	u, err := url.Parse("//192.168.2.2:6688")
 	if nil != err {
 		panic(err)
 	}
